cb11563ba66136ef96b07e90e74839d8dcd47e6b
MrRari14 <ber.97ferrari@gmail.com>
2023-09-17 16:02:12 +1200

    Removing declaration in config

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,7 @@
 module.exports = {
     verbose: true,
     preset: "jest-puppeteer",
-    testEnvironment: "puppeteer", // Makes sure it uses Puppeteer as the browser tester (Jest is the framework !=)
-    jest: {
-        "testMatch": ["<rootDir>/tests/**/*.js"]
-    },  
+    testEnvironment: "puppeteer", // Makes sure it uses Puppeteer as the browser tester (Jest is the framework !=)  
     transform: {
         '^.+\\.jsx?$': 'babel-jest',    
     },
