1eeffec3904df4b384c3455d872716594a0138b0
MrRari14 <ber.97ferrari@gmail.com>
2023-09-17 16:00:59 +1200

    Telling jest where to look for test file

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     verbose: true,
     preset: "jest-puppeteer",
     testEnvironment: "puppeteer", // Makes sure it uses Puppeteer as the browser tester (Jest is the framework !=)
-    "jest": {
+    jest: {
         "testMatch": ["<rootDir>/tests/**/*.js"]
     },  
     transform: {
