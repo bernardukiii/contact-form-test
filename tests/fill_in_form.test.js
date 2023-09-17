fc6b3f20749639df876d61fa5afc646e3fd5ee42
MrRari14 <ber.97ferrari@gmail.com>
2023-09-18 10:46:36 +1200

    Switching to 'real' URL

--- tests/fill_in_form.test.js
+++ tests/fill_in_form.test.js
@@ -8,7 +8,7 @@ describe('Open page in browser', () => {
   beforeAll( async () => {
     browser = await puppeteer.launch({ headless: true })
     page = await browser.newPage()
-    await page.goto('http://localhost:3000/');
+    await page.goto('https://bdki-portfolio.vercel.app/');
   
   })
 
